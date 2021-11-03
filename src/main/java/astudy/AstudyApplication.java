935d9eb5f0cfb398074d0fcb53705f93bbfd17cc
vohuong108 <huongvovan123@gmail.com>
2021-11-04 00:59:53 +0700

    initial source code

---
+++ src/main/java/astudy/AstudyApplication.java
@@ -0,0 +1,13 @@
+package astudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AstudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AstudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/astudy/AstudyApplicationTests.java
@@ -0,0 +1,13 @@
+package astudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AstudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
